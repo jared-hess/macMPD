ea99c9d6f698ecd5e90f44a48580e0f23d259481
Jared Hess <jhess@sjcmac82m3g8wm.local>
2017-02-17 15:22:31 -0800

    Initial Commit

---
+++ macMPD/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macMPD
+//
+//  Created by Jared Hess on 2/17/17.
+//  Copyright © 2017 Jared Hess. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macMPD/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macMPD
+//
+//  Created by Jared Hess on 2/17/17.
+//  Copyright © 2017 Jared Hess. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
